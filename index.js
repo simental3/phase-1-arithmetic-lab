89d67a61ea7ed09d99449ae39d4f9898a35b66a9
Anthony Simental <anthonysimental12@gmail.com>
2022-11-30 09:46:20 -0800

    commit tree d8883e955e5ee6cdac8925585085ad29579fac83

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 6;
+
+// Equation that multiplies a result that equals 62
+const multiply = num1 * num2;
+
+// Generates a random integer > 0
+const random = Math.ceil(Math.random() * 10);
+
+// Equation that calculates a remainder that equals 4
+const mod = num3 % num4;
+
+// Returns 20 as the highest number in the set
+const max = Math.max(1, 4, 8, 12, 16, 20);
+
+
+// Tests code
+console.log(multiply);
+console.log(random);
+console.log(mod);
+console.log(max);
